28b19e237123c96648912fcf52bf0169cbe6ab2c
dennissammy77 <dennissammy77@gmail.com>
2025-03-30 17:34:55 +0300

    practice challenge

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(username, useremail) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({ name: username, email: useremail })
+    };
+    
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        console.log(object);
+        document.body.innerHTML=object.id
+        return object;
+      })
+      .catch(function (error) {
+        console.error(error.message);
+        document.body.innerHTML=error.message
+      });
+}
